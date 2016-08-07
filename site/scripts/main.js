1528200f4b058d8c7edd531d794800b51648d697
iliya <iliyuskeisar@gmail.com>
2016-08-07 14:52:32 +0300

    added light box

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -51,6 +51,7 @@ Site.is_mobile = function() {
 Site.on_load = function() {
 	if (Site.is_mobile())
 		Site.mobile_menu = new Caracal.MobileMenu();
+	Site.lightbox = new LightBox('a.lightbox',  false,  false,  true);
 };
 
 
